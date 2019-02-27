2f8ff4d19402d5643e55f7d54f517a93e687e8cd
Huanrong <caohrong@live.com>
2019-02-27 10:42:08 +0800

    first commit

---
+++ TimeManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimeManager
+//
+//  Created by Huanrong on 2/27/19.
+//  Copyright © 2019 Huanrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
