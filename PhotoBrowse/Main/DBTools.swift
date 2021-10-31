4d4d7a8413f1b83753b2fbd62d835a88fb2526a6
Huanrong <caohrong@live.com>
2021-10-31 19:58:35 +0800

    更换成FMDB数据库

--- PhotoBrowse/Main/DBTools.swift
+++ PhotoBrowse/Main/DBTools.swift
@@ -7,51 +7,85 @@
 
 import Foundation
 import Photos
-import SQLite
+import FMDB
 
 class DBTools : NSObject {
-    fileprivate var db:Connection!
+    fileprivate var db:FMDatabase!
     
     override init() {
-        
+        var fileURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        fileURL.appendPathComponent("db.sqlite3")
+        db = FMDatabase(url: fileURL)
     }
     
     func saveAllPhotosResult(fetchResult: PHFetchResult<PHAsset>) {
-        print("saveAllPhotosResult")
-        var dburl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
-        dburl.appendPathComponent("db.sqlite3")
-        print(dburl.absoluteString)
-        
-        do {
-            db = try Connection(dburl.absoluteString)
-        } catch {
-            print(error)
+        guard db.open() else {
+            print("Unable to open database")
+            return
         }
-        
-        //Create Table
-        let t_photos = Table("Photos")
-        let id = Expression<String>("id")
-        let checkDate = Expression<String?>("checkDate")
 
         do {
-//            try db.run(t_photos.create { t in
-//            t.column(id, primaryKey: true)
-//            t.column(checkDate)
-//            })
+            try db.executeUpdate("create table if not exists Photos(id integer primary key autoincrement, identifier test, checkDate text)", values: nil)
             
-            for i in 0..<fetchResult.count {
-                let asset:PHAsset = fetchResult.object(at: i)
-                let insert = t_photos.insert(id <- asset.localIdentifier, checkDate <- "20211031")
-                let rowid = try db.run(insert)
-                print(rowid)
-    //            if let location = asset.location {
-    //                print(location)
-    //            }
+            //TODO
+            //确定是否需要更新
+            
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
+            let dateString = formatter.string(from: Date())
+            
+//            for i in 0..<fetchResult.count {
+//                let asset:PHAsset = fetchResult.object(at: i)
+//                try db.executeUpdate("insert into Photos (identifier, checkDate) values (?, ?)", values: [asset.localIdentifier, dateString])
+//            }
+            
+            let rs = try db.executeQuery("select count(*) from Photos", values: nil)
+            while (rs.next()) {
+                print(rs.int(forColumn: "count"))
             }
         } catch {
-            print(error)
+            print("报错: \(error.localizedDescription)")
         }
+
+        db.close()
     }
+    
+//    func saveAllPhotosResult(fetchResult: PHFetchResult<PHAsset>) {
+//        print("saveAllPhotosResult")
+//        var dburl = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+//        dburl.appendPathComponent("db.sqlite3")
+//        print(dburl.absoluteString)
+//
+//        do {
+//            db = try Connection(dburl.absoluteString)
+//        } catch {
+//            print(error)
+//        }
+//
+//        //Create Table
+//        let t_photos = Table("Photos")
+//        let id = Expression<String>("id")
+//        let checkDate = Expression<String?>("checkDate")
+//
+//        do {
+////            try db.run(t_photos.create { t in
+////            t.column(id, primaryKey: true)
+////            t.column(checkDate)
+////            })
+//
+//            for i in 0..<fetchResult.count {
+//                let asset:PHAsset = fetchResult.object(at: i)
+//                let insert = t_photos.insert(id <- asset.localIdentifier, checkDate <- "20211031")
+//                let rowid = try db.run(insert)
+//                print(rowid)
+//    //            if let location = asset.location {
+//    //                print(location)
+//    //            }
+//            }
+//        } catch {
+//            print(error)
+//        }
+//    }
     
     func addLocationInfoLabel(asset:PHAsset) {
         
